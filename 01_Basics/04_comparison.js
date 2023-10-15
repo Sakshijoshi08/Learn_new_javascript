989e116e702c82058e8fc2b728db3c7733953028
Sakshijoshi08 <147054279+Sakshijoshi08@users.noreply.github.com>
2023-10-15 12:16:50 +0000

    04_comparision.js

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2==3);
+// console.log(2>=3);
+// console.log(2<=3);
+// console.log(2!=3);
+
+//  dataType should be same 
+// console.log("1" < 2);
+// console.log("01" < 2);
+// *****************************************************************************
+// avoid tis type of code 
+// console.log(null > 0 );
+// console.log(null == 0);
+// console.log(null >= 0); // null is converted into 0 therefore  it is true 
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// ****************************************************************************
+//  Strict check  === it checks datatype also 
+// console.log("2" === 2);
