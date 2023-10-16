4ab85222cbd68d03c66b6d2dac54bfdd3b351367
Sakshijoshi08 <147054279+Sakshijoshi08@users.noreply.github.com>
2023-10-16 16:20:44 +0000

    01_array.js

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,14 @@
+const myArr = [0,1,2,3,4,5,true, "sakshi"] 
+// The Array object, as with arrays in other programming languages,
+//  enables storing a collection of multiple items under a single variable name,
+//  and has members for performing common array operations.
+
+console.log(myArr[0]); // indexing starts with [0] 
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+// shallow copy and deep copy 
+
+const heros =["sakshi", "sonu"]
+const mynewArray = new Array(1,2,3,4)
+console.log(mynewArray[0]);
