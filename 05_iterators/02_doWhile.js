1b323a553ea245b867748301a609e8171509cb4d
Sakshijoshi08 <147054279+Sakshijoshi08@users.noreply.github.com>
2023-10-20 09:29:39 +0000

    02_doWhile.js

---
+++ 05_iterators/02_doWhile.js
@@ -0,0 +1,27 @@
+// while 
+
+let index = 0 
+while (index <= 10) {
+    // console.log(`value of index s ${index}`);
+ index = index +2;   
+}
+
+// array with while loop
+
+ const myArray = [" sakshi", " pal", "developer"]
+let arr = 0 
+ while (arr<myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr +1
+ }
+
+//  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// do-while Loop 
+
+let score = 1 // initialization
+do {
+    // console.log(`Score is ${score}`);
+    score++ // increment decreament 
+} while (score <= 10); // condition
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
