2fe068eee7f34fd8f8d9b19040e0469a2b87e613
Sakshijoshi08 <147054279+Sakshijoshi08@users.noreply.github.com>
2023-10-20 15:39:55 +0000

    document.js

---
+++ 06_Dom/consolepratice.js
@@ -0,0 +1,18 @@
+document.getElementById('title')
+document.getElementById('title').id
+document.getElementById('title').className
+document.getElementById('title').getAttribute('id')
+document.getElementById('title').getAttribute('class')
+document.getElementById('title').setAttribute('class','test') // it overlaps heading 
+document.getElementById('title').setAttribute('class','test heading')
+document.getElementById('title')
+{/* <h1 id=​"title" class=​"test heading">​Dom Learning on youtube​</h1>​
+title
+<h1 id=​"title" class=​"test heading">​Dom Learning on youtube​</h1>​
+title.style.backgroundColor ='red' 
+'red'
+title.style.padding ="15px"
+'15px'
+title.style.borderRadius = "15px"
+'15px' */}
+
