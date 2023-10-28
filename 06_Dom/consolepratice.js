92a92bd6743d5ca04d1cc3f3fa7a1ca8cc9617cc
Sakshijoshi08 <147054279+Sakshijoshi08@users.noreply.github.com>
2023-10-28 12:41:28 +0000

    new

--- 06_Dom/consolepratice.js
+++ 06_Dom/consolepratice.js
@@ -16,3 +16,15 @@ title.style.padding ="15px"
 title.style.borderRadius = "15px"
 '15px' */}
 
+// for conversion html collection into array the method is :
+// Array.from(listname)
+
+// document.querySelectorAll('h2')
+// const allH2 = document.querySelectorAll('h2')
+// allH2[1].innerText
+// document.querySelectorAll(".mw-headline")
+// myH2[0].innerText
+// myH2.forEach(function (h) {
+//     h.style.color ='black';
+//         h.style.backgroundColor ='green';
+// })
